f45b855afeb915cd94e87839046d1d11f53696e6
Innocent Thomas <innthomas00158@gmail.com>
2020-10-09 04:25:24 +0100

    ...

--- lib/screens/buildShowDialog.dart
+++ lib/screens/buildShowDialog.dart
@@ -2,20 +2,32 @@ import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 FirebaseFirestore firestore = FirebaseFirestore.instance;
+CollectionReference users = firestore.collection("bankAccounts");
+TextEditingController _amtController = TextEditingController();
 
 Future buildShowDialog(BuildContext context) {
   return showDialog(
       context: context,
       builder: (_) => new AlertDialog(
             title: new Text("Material Dialog"),
-            content: Text("data"),
+            content: TextField(
+              decoration: InputDecoration(hintText: "amount"),
+              controller: _amtController,
+            ),
             actions: <Widget>[
               FlatButton(
                 child: Text('Close me!'),
                 onPressed: () {
+                  _updateData();
                   Navigator.of(context).pop();
                 },
               )
             ],
           ));
+}
+
+void _updateData() async {
+  users
+      .doc("acctNumber")
+      .update({"acctDeposit": double.parse(_amtController.text)});
 }
