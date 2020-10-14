4557fb9a016714c311ab3eed08a84677e7c394df
Innocent Thomas <innthomas00158@gmail.com>
2020-10-14 06:39:57 +0100

    added dialog edit

--- lib/screens/my_show_dialog.dart
+++ lib/screens/my_show_dialog.dart
@@ -1,19 +1,50 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
+TextEditingController amtController = TextEditingController();
 Future myShowDialog(context, DocumentSnapshot document) {
   return showDialog(
-      context: context,
-      builder: (_) => new AlertDialog(
-            title: new Text(document["acctName"]),
-            content: new Text("Hey! I'm Coflutter!"),
-            actions: <Widget>[
-              FlatButton(
-                child: Text('Close me!'),
-                onPressed: () {
-                  Navigator.of(context).pop();
-                },
-              )
-            ],
-          ));
+    context: context,
+    builder: (_) => new AlertDialog(
+      title: Column(
+        children: [
+          new Text(
+            document["acctName"],
+            style: TextStyle(fontFamily: "Pacifico", fontSize: 25.0),
+          ),
+          Text(
+            document["acctNumber"].toString(),
+          ),
+        ],
+      ),
+      content: new TextField(
+        decoration: InputDecoration(
+          labelText: "amount",
+        ),
+      ),
+      actions: <Widget>[
+        RaisedButton(
+          color: Colors.red,
+          child: Text('wDraw'),
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+        ),
+        RaisedButton(
+          color: Colors.green,
+          child: Text('Deposit'),
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+        ),
+        RaisedButton(
+          color: Colors.amber,
+          child: Icon(Icons.close),
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+        ),
+      ],
+    ),
+  );
 }
